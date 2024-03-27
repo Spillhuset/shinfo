6092d768c8e97ca8ba98e01865fc16e77a120767
Glenn <glenn@not-tech.support>
2024-03-27 18:42:04 +0000

    fix: wait for system to go back online before refreshing after outage
    Signed-off-by: Glenn <glenn@not-tech.support>

--- src/static/js/viewer/ws.js
+++ src/static/js/viewer/ws.js
@@ -2,27 +2,50 @@ const wsUrl = new URL(window.location.href);
 wsUrl.protocol = wsUrl.protocol.replace("http", "ws");
 wsUrl.pathname = wsUrl.pathname.replace("viewer", "ws");
 
-let wsPulseInterval;
 
-const ws = new WebSocket(wsUrl.href);
-ws.onopen = () => {
-  console.log("Screen WS connected");
-  wsPulseInterval = setInterval(() => ws.send(JSON.stringify({ type: "pulse" })), 30000);
+let ws = createWs();
+
+function createWs() {
+  let wsPulseInterval;
+  const newWs = new WebSocket(wsUrl.href);
+  newWs.onopen = () => {
+    console.log("Screen WS connected");
+    wsPulseInterval = setInterval(() => newWs.send(JSON.stringify({ type: "pulse" })), 30000);
+  }
+
+  newWs.onclose = () => {
+    console.log("Screen WS disconnected");
+    clearInterval(wsPulseInterval);
+    ws = null;
+    reloadWhenOkResponse();
+  }
+
+  newWs.onerror = (e) => {
+    console.error(e);
+    clearInterval(wsPulseInterval);
+    ws = null;
+    reloadWhenOkResponse();
+  }
+
+  newWs.onmessage = (e) => {
+    const data = JSON.parse(e.data);
+    onWsMessage(data);
+    if (data.type === "reload_screen") window.location.reload();
+  }
+
+  return newWs;
 }
 
-ws.onclose = () => {
-  console.log("Screen WS disconnected");
-  clearInterval(wsPulseInterval);
-  setTimeout(window.location.reload.bind(window.location), 5000);
+async function reloadWhenOkResponse() {
+  const testUrl = new URL(window.location.href);
+  do {
+    console.log("Waiting for server to be back up...");
+    await sleep();
+  } while (await fetch(testUrl.href).then(response => response.ok).catch(() => false) === false);
+  await sleep();
+  window.location.reload();
 }
 
-ws.onerror = (e) => {
-  console.error(e);
-  setTimeout(window.location.reload.bind(window.location), 5000);
-}
-
-ws.onmessage = (e) => {
-  const data = JSON.parse(e.data);
-  onWsMessage(data);
-  if (data.type === "reload_screen") window.location.reload();
+async function sleep(ms = 5_000) {
+  await new Promise(r => setTimeout(r, ms));
 }
